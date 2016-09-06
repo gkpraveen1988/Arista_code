0168f0345e0ce981c915fb84b00ca34b39409bc1
RadheyReddy <radheytinku@gmail.com>
2016-09-06 16:09:15 +0530

    Updated the host name for mysql
    Updated the  host name as benchmarkdb.aristanetworks.com for mysql connection

---
+++ app/server/js/dbConfig.js
@@ -0,0 +1,25 @@
+/**
+ * Contains all the files for server side scripting.
+ *
+ * @module server
+ */
+ /**
+ * This JavaScript File contains all the configurations related to mysql DataBase.
+ *
+ * @class dbConfig.js
+ * @constructor
+ */
+
+exports.dbPool = function(){
+var DB = require("../lib/node_modules/mysql");
+var pool = DB.createPool({
+        connectTimeout  : 60 * 60 * 1000,
+        aquireTimeout   : 60 * 60 * 1000,    
+		host : "benchmarkdb.aristanetworks.com",
+		database : "benchmark",
+		user : "arastra",
+		port : 3306,
+		timezone : "UTC"
+	});
+return pool;	
+}
